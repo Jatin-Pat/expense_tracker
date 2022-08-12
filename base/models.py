4b34ccdd8ff6636d2c29549cd4314e90e6d3ce15
Jatin Patel <jatin_pat@outlook.com>
2022-08-12 07:26:10 -0400

    fixed database error and migrated models

--- base/models.py
+++ base/models.py
@@ -5,22 +5,31 @@ from django.core.exceptions import ValidationError
 def validate_positive(value):
     if value < 0.0:
         raise ValidationError(
-            ('%(value)s is not a positive number'),
-            params={'value': value},
+            ("%(value)s is not a positive number"),
+            params={"value": value},
         )
 
 class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.Choices = [('housing', 'Housing'), ('transportation', 'Transportation'), ('food', 'Food'), ('utilities', 'Utilities'), ('insurance', 'Insurance'), ('medical', 'Medical & Healthcare'), ('debt', 'Dept Payments'), ('personal', 'Personal Spending')]
+    category_choices = [
+        ("housing", "Housing"),
+        ("transportation", "Transportation"),
+        ("food", "Food"),
+        ("utilities", "Utilities"),
+        ("insurance", "Insurance"),
+        ("medical", "Medical & Healthcare"),
+        ("debt", "Dept Payments"),
+        ("personal", "Personal Spending")]
+    category = models.CharField(max_length=15, choices=category_choices, default="housing")
     sub_category = models.CharField(max_length=200, null=True, blank=True)
     amount = models.FloatField(validators=[validate_positive])
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.category + ':' + self.amount + '$'
+        return self.category + ":" + self.amount + "$"
 
     class Meta:
-        ordering = ['date']
+        ordering = ["date"]
 
 
 
