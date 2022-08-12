2cca10fd4385f79e07c2ec1da3989e4bac6d6460
Jatin Patel <jatin_pat@outlook.com>
2022-08-12 06:42:43 -0400

    initialized database

--- base/models.py
+++ base/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+def validate_positive(value):
+    if value < 0.0:
+        raise ValidationError(
+            ('%(value)s is not a positive number'),
+            params={'value': value},
+        )
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.Choices = [('housing', 'Housing'), ('transportation', 'Transportation'), ('food', 'Food'), ('utilities', 'Utilities'), ('insurance', 'Insurance'), ('medical', 'Medical & Healthcare'), ('debt', 'Dept Payments'), ('personal', 'Personal Spending')]
+    sub_category = models.CharField(max_length=200, null=True, blank=True)
+    amount = models.FloatField(validators=[validate_positive])
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.category + ':' + self.amount + '$'
+
+    class Meta:
+        ordering = ['date']
+
+
+
