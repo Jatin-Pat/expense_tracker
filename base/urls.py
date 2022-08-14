80fe903c808d412d7bdef8be19c7bc491ab086fa
Jatin Patel <jatin_pat@outlook.com>
2022-08-14 15:52:14 -0400

    Connected Login and Logout views

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ExpenseListView, ExpenseUpdateView, ExpenseDeleteView
+from .views import ExpenseListView, ExpenseUpdateView, ExpenseDeleteView, UserLoginView, UserLogoutView
 
 
 urlpatterns = [
+    path('login/', UserLoginView.as_view(), name = 'Expense-login-view'),
+    path('logout/', UserLogoutView.as_view(), name = 'Expense-logout-view'),
     path('', ExpenseListView.as_view(), name = 'Expense-list-view'),
     path('expense-update/<int:pk>/', ExpenseUpdateView.as_view(), name = 'Expense-update-view'),
     path('expense-delete-confirmation/<int:pk>/', ExpenseDeleteView.as_view(), name = 'Expense-delete-confirmation-view')
--- base/views.py
+++ base/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import Expense
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+class UserLoginView(LoginView):
+    fields = '__all__'
+    template_name = 'base/login.html'
+    redirect_authenticated_user = True
+    LoginView.next_page = reverse_lazy('Expense-list-view')
+    
+class UserLogoutView(LogoutView):
+    next_page = 'Expense-login-view'
 
 class ExpenseListView(CreateView, ListView):
     model = Expense
