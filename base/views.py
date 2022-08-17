7321ea08442772ca38d73e57d021f88697e4c6f5
Jatin Patel <jatin_pat@outlook.com>
2022-08-17 06:50:47 -0400

    Restricted views to non-logged in users

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from .models import Expense
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
@@ -13,18 +13,18 @@ class UserLoginView(LoginView):
 class UserLogoutView(LogoutView):
     next_page = 'Expense-login-view'
 
-class ExpenseListView(CreateView, ListView):
+class ExpenseListView(LoginRequiredMixin, CreateView, ListView):
     model = Expense
     fields = '__all__'
     template_name = 'base/expense.html'
     success_url = reverse_lazy('Expense-list-view')
 
-class ExpenseUpdateView(UpdateView):
+class ExpenseUpdateView(LoginRequiredMixin, UpdateView):
     model = Expense
     fields = '__all__'
     success_url = reverse_lazy('Expense-list-view')
 
-class ExpenseDeleteView(DeleteView):
+class ExpenseDeleteView(LoginRequiredMixin, DeleteView):
     model = Expense
     context_object_name = 'expense'
     success_url = reverse_lazy('Expense-list-view')
--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -106,6 +106,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'Expense-login-view'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
